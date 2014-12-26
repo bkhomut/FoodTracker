83ed067ca5ec84dc9b75c9cc1b43e5de68f3da9c
bkhomut <boris@Boriss-MacBook-Air.local>
2014-12-26 14:15:06 -0500

    first commit foodTracker

---
+++ FoodTracker/DataConrtroller.swift
@@ -0,0 +1,42 @@
+//
+//  DataConrtroller.swift
+//  FoodTracker
+//
+//  Created by Boris Khomut on 12/22/14.
+//  Copyright (c) 2014 Boris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class DataController {
+    
+    class func jsonAsUSDAIdAndNameSearchResults (json : NSDictionary) -> [(name : String, idValue: String)] {
+        
+        var usdaItemsSearchResults: [(name : String, idValue : String)] = []
+        var searchResult: (name : String, idValue : String)
+        
+        if json["hits"] != nil {
+            let results : [AnyObject] = json["hits"]! as [AnyObject]
+
+            for itemDictionary in results {
+                
+                if itemDictionary["_id"] != nil {
+                    if itemDictionary["fields"] != nil {
+                        let fieldsDictionary = itemDictionary["fields"] as NSDictionary
+                        if fieldsDictionary["item_name"] != nil {
+                            let idValue: String = itemDictionary["_id"]! as String
+                            let name: String = fieldsDictionary["item_name"]! as String
+                            searchResult = (name : name, idValue : idValue)
+                            usdaItemsSearchResults += [searchResult]
+                            
+                        }
+                    }
+                }
+            }
+        }
+        
+        return usdaItemsSearchResults
+    }
+}
---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Boris Khomut on 12/20/14.
+//  Copyright (c) 2014 Boris. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func eatItBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
+
+
+    
+}
